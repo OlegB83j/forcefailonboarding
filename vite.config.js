cd5b0c231fb01b7c4a17739ada30071fd9b5b683
OlegB83 <49716669+OlegB83@users.noreply.github.com>
2025-10-29 12:10:39 +0100

    3

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true,
-    allowedHosts: true
+    port: 3000
   }
 })
